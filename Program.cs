5d312e58ebc7becbb277757e5439bada6315f4b4
Mehmet Yiğit Kasap <71186488+thejarrus@users.noreply.github.com>
2025-07-27 10:29:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bir gün girin:");
+        string gun = Console.ReadLine();
+        gun = gun.ToLower();
+
+        switch (gun)
+        {
+            case "pazartesi":
+                Console.WriteLine("Haftanın 1. günü");
+                break;
+
+            case "salı":
+                Console.WriteLine("Haftanın 2. günü");
+                break;
+
+            case "çarşamba":
+                Console.WriteLine("Haftanın 3. günü");
+                break;
+
+            case "perşembe":
+                Console.WriteLine("Haftanın 4. günü");
+                break;
+
+            case "cuma":
+                Console.WriteLine("Haftanın 5. günü");
+                break;
+
+            case "cumartesi":
+                Console.WriteLine("Haftanın 6. günü");
+                break;
+
+            case "pazar":
+                Console.WriteLine("Haftanın 7. günü");
+                break;
+
+            default:
+                Console.WriteLine("Geçersiz gün girdiniz.");
+                break;    
+        }
+    }
+}
+
